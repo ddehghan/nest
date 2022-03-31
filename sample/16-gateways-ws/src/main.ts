bab02aff816e1a6581c8587e64240b7ad6d837ba
David Dehghan <ddehghan@gmail.com>
2022-03-30 20:28:01 -0700

    added ssl to example - not working

--- sample/16-gateways-ws/src/main.ts
+++ sample/16-gateways-ws/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    // ssl certs that are in sample/16-gateways-ws/certs
+    key: fs.readFileSync('certs/privkey.pem'),
+    cert: fs.readFileSync('certs/fullchain.pem'),
+  };
+
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+
   app.useWebSocketAdapter(new WsAdapter(app));
 
   await app.listen(3000);
